e9dc756052baebbe27ca233cca7b8a54a608792a
Алексей Попов <MrAlekZzzz@gmail.com>
2023-06-26 23:39:51 +0500

    Добавил штуки для продуктов
    Signed-off-by: Алексей Попов <MrAlekZzzz@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {IProduct} from "./models/product";
+import {products as data} from "./data/products";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'test-angular-app';
+  title = 'SuperLexa'
+
+  products: IProduct[] = data
 }
